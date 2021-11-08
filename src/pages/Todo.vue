4d26c4a26523ed7a9d9013191a674e8a7dce1352
Shoko118 <secast118@gmail.com>
2021-11-08 18:12:25 +0800

    updated rating

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,28 +1,42 @@
 <template>
   <h1>this is the todo page</h1>
+  <!-- input of text -->
   <input
     type="text"
     placeholder="enter a list"
     v-model="newList"
     @keyup.enter.prevent="addButton"
   />
+  <!-- input of number -->
+  <input
+    type="number"
+    placeholder="enter rating"
+    v-model="newRating"
+    min="1"
+    max="5"
+  />
   <button @click.prevent="addButton">Add</button>
-  <!-- loop todos -->
+  <button v-if="deleteArray.length > 0" @click.prevent="undoDeletedTodos">
+    Undo deleted todos
+  </button>
+  <!-- loop todos =========-->
   <div
     v-for="(todo, index) in filterTodos"
     :key="index"
     :class="{ done: todo.completed }"
   >
+    <b>Rating</b>: {{ todo.rating }}
     {{ todo.list }}
     <button @click.prevent="removeTodos(index)">X</button>
     <input type="checkbox" v-model="todo.completed" />
   </div>
-  <!-- select options -->
+  <!-- select options============== -->
   <select name="Filter" v-model="filters">
     <option value="All">All</option>
     <option value="Delete">Delete</option>
     <option value="Complete">Complete</option>
     <option value="Incomplete">Incomplete</option>
+    <option value="ratingAscend">Sort ascend by importance</option>
   </select>
 </template>
 
@@ -31,17 +45,16 @@ import { computed, ref } from "vue";
 export default {
   setup() {
     const newList = ref("");
-    const deleteArray = ref([]);
+    const newRating = ref(1);
+    let deleteArray = ref([]);
     const filters = ref("All");
     const todos = ref([
-      { list: "swimming", completed: false, incomplete: false },
-      { list: "basketball", completed: false, incomplete: false },
-      { list: "badminton", completed: false, incomplete: false },
-      { list: "volleyball", completed: false, incomplete: false },
-      { list: "hiking", completed: false, incomplete: false },
+      { rating: 1, list: "swimming", completed: false, incomplete: false },
+      { rating: 2, list: "basketball", completed: false, incomplete: false },
+      { rating: 3, list: "badminton", completed: false, incomplete: false },
     ]);
 
-    // computed
+    // computed ============
     const filterTodos = computed(() => {
       if (filters.value === "All") return todos.value;
       else if (filters.value === "Delete") return deleteArray.value;
@@ -49,13 +62,16 @@ export default {
         return todos.value.filter((todo) => todo.completed === true);
       } else if (filters.value === "Incomplete") {
         return todos.value.filter((todo) => todo.incomplete === false);
+      } else if (filters.value === "ratingAscend") {
+        return todos.value.sort((a, b) => a.rating - b.rating);
       }
     });
 
-    // functions
+    // functions============
     function addButton() {
       todos.value.push({
         list: newList.value,
+        rating: newRating.value,
       });
       newList.value = "";
     }
@@ -65,14 +81,23 @@ export default {
       todos.value.splice(index, 1);
     }
 
+    function undoDeletedTodos() {
+      deleteArray.value.forEach((todo) => {
+        todos.value.push(todo);
+      });
+      deleteArray.value = [];
+    }
+
     return {
       newList,
+      newRating,
       deleteArray,
       filters,
       todos,
       filterTodos,
       addButton,
       removeTodos,
+      undoDeletedTodos,
     };
   },
 };
